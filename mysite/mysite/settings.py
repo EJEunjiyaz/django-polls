81f8193be43535eb4eef2e9bc8d7fcf1d37dc17e
Vichyawat Nakarugsa <vichyawat.n@ku.th>
2019-09-16 16:30:06 +0700

    Remove hosted url

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,10 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-
 
 # Application definition
 
