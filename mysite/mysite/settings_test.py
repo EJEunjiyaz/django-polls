a6fa53c67c4149edd68032ab8d818f098be7bdfe
Vichyawat Nakarugsa <vichyawat.n@ku.th>
2019-09-16 15:43:32 +0700

    change setting_test

--- mysite/mysite/settings_test.py
+++ mysite/mysite/settings_test.py
@@ -1,31 +1,17 @@
-# coding: utf-8
-from decouple import config
-from django.urls import path
-# from dj_database_url import parse as db_url
-
 import os
-
+from decouple import config, Csv
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-DEBUG = config('DEBUG', default=False, cast=bool)
-TEMPLATE_DEBUG = DEBUG
-
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=True, cast=bool)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '',
     }
 }
-
-TIME_ZONE = 'Asia/Bangkok'
-USE_L10N = True
-USE_TZ = True
-
-SECRET_KEY = config('SECRET_KEY')
-
-EMAIL_HOST = config('EMAIL_HOST', default='localhost')
-EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
