19add2cbacbff962ae219834073d2e9a1bcf8951
Vichyawat Nakarugsa <vichyawat.n@ku.th>
2019-11-04 12:16:00 +0700

    redirect after login

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,6 +16,7 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+LOGIN_REDIRECT_URL = '/polls/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
