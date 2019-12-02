c36cccb7936db1b7ad4298626a3dda4884dc9c5d
Vichyawat Nakarugsa <vichyawat.n@ku.th>
2019-12-02 17:15:53 +0700

    Add chromedriver and fix some bugs

--- mysite/polls/tests/functional_test.py
+++ mysite/polls/tests/functional_test.py
@@ -1,14 +1,20 @@
-from selenium import webdriver
 import requests
+from selenium import webdriver
+from django.test import TestCase
+from django.utils import timezone
+
+# from polls.models import Question, Choice
+
 
 def get_links(url):
     """Find all links on page at the given url.
        Return a list of all link addresses, as strings.
     """
-    browser = webdriver.Chrome(executable_path=r'C:/chromedriver/chromedriver.exe')
+    browser = webdriver.Chrome('mysite/polls/tests/chromedriver/chromedriver.exe')
     browser.get(url)
     elements = browser.find_elements_by_tag_name('a')
     links = [element.get_attribute('href') for element in elements]
+    browser.close()
     return links
 
 def invalid_urls(urllist):
@@ -27,7 +33,7 @@ def invalid_urls(urllist):
 def main():
     """Method for testing question 1-4"""
 
-    browser = webdriver.Chrome(executable_path=r'C:/chromedriver/chromedriver.exe')
+    browser = webdriver.Chrome('mysite/polls/tests/chromedriver/chromedriver.exe')
     browser.get('https://cpske.github.io/ISP/')
     elements = browser.find_elements_by_tag_name('a')
     for element in elements:
@@ -42,5 +48,23 @@ def main():
         print("Bad URLs Here")
         print(*invalid_url_list, sep = "\n")
     browser.close()
+
+
+class PollsTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """This method is run only once, before any tests are run."""
+        q1 = Question(question_text='Who is your favorite singer in TWICE?', pub_date=timezone.now())
+        q1.save()
+        c1 = Choice(question=q1, choice_text='Nayeon')
+        c2 = Choice(question=q1, choice_text='Nayeon', votes=2)
+        c1.save()
+        c2.save()
+
+    def setUp(self):
+        """This method is called before every test."""
+        self.browser = webdriver.Chrome('mysite/polls/tests/chromedriver/chromedriver.exe')
+
+    # def test_page(self):
 
 main()
